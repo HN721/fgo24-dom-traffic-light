01c84e70b2bfb8df396fd897c446160c4bf6f284
HN721 <hosea1422@gmail.com>
2025-05-07 13:41:02 +0700

    refactor: code in script.js to more clean code

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-const red = document.querySelector(".red");
-const yellow = document.querySelector(".yellow");
-const green = document.querySelector(".green");
-
+const light = {
+  red: document.querySelector(".red"),
+  yellow: document.querySelector(".yellow"),
+  green: document.querySelector(".green"),
+};
 let current = "red";
 
 function switchLight() {
-  red.classList.remove("active");
-  yellow.classList.remove("active");
-  green.classList.remove("active");
+  Object.values(light).forEach((light) => light.classList.remove("active"));
   if (current === "red") {
-    red.classList.add("active");
+    light.red.classList.add("active");
     current = "yellow";
     setTimeout(switchLight, 3000);
   } else if (current === "yellow") {
-    yellow.classList.add("active");
+    light.yellow.classList.add("active");
     current = "green";
     setTimeout(switchLight, 2000);
   } else if (current === "green") {
-    green.classList.add("active");
+    light.green.classList.add("active");
     current = "red";
     setTimeout(switchLight, 3000);
   }
