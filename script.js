3fa5de806682b72977696d0e3028b6ba9fb42ebf
HN721 <hosea1422@gmail.com>
2025-05-07 13:09:22 +0700

    feat: add traffic-light function

---
+++ script.js
@@ -0,0 +1,26 @@
+const red = document.querySelector(".red");
+const yellow = document.querySelector(".yellow");
+const green = document.querySelector(".green");
+
+let current = "red";
+
+function switchLight() {
+  red.classList.remove("active");
+  yellow.classList.remove("active");
+  green.classList.remove("active");
+  if (current === "red") {
+    red.classList.add("active");
+    current = "yellow";
+    setTimeout(switchLight, 3000);
+  } else if (current === "yellow") {
+    yellow.classList.add("active");
+    current = "green";
+    setTimeout(switchLight, 1000);
+  } else if (current === "green") {
+    green.classList.add("active");
+    current = "red";
+    setTimeout(switchLight, 3000);
+  }
+}
+
+switchLight();
